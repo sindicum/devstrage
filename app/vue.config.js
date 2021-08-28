3266321ea8bdd5e71de3f3fb046e64188a66ea7d
M.Goto <sindicum@icloud.com>
2021-08-28 10:25:40 +0900

    commit

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = {
   // outputDir: '../html'
-  outputDir: '../docs',
-  // outputDir: path.resolve(__dirname, "../docs")
-  publicPath: './',
-  assetsDir: './',
+  // outputDir: '../docs',
+  outputDir: path.resolve(__dirname, "../docs")
+  // publicPath: './',
+  // assetsDir: './',
 }
 
