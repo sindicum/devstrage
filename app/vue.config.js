a5644a5398194cceb452d8fb25c85d071dc0365e
M.Goto <sindicum@icloud.com>
2021-08-28 10:28:08 +0900

    modify vue.config.js

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  // outputDir: '../html'
   // outputDir: '../docs',
-  outputDir: path.resolve(__dirname, "../docs")
-  // publicPath: './',
-  // assetsDir: './',
+  outputDir: path.resolve(__dirname, "../docs"),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/devstrage/'
+    : '/'
 }
 
